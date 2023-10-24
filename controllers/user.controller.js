c3a1535e10f1178f2f6bdd3019453368de2c08da
adamrayw <raywibowo68@gmail.com>
2023-10-25 01:11:59 +0700

    add sendEmail function, forgotPassword function

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,9 @@
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require('@prisma/client');
+const { default: axios } = require('axios');
 
 const prisma = new PrismaClient()
 const bcrypt = require('bcrypt')
-const jwt    = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
 // JWT env
 const secretKey = process.env.JWT_SECRET;
@@ -18,10 +19,10 @@ const contohResponse = (req, res) => {
 }
 
 // Register New User
-const registerUser = async(req, res) => {
+const registerUser = async (req, res) => {
     const { fullname, email, password } = req.body;
 
-    if(!fullname || !email || !password) {
+    if (!fullname || !email || !password) {
         return res.status(400).json({ error: 'All must be filled' })
     }
 
@@ -34,8 +35,8 @@ const registerUser = async(req, res) => {
             where: { email },
         })
 
-        if(duplicateUser) {
-            return res.status(400).json({ error: 'Email already registered'})
+        if (duplicateUser) {
+            return res.status(400).json({ error: 'Email already registered' })
         }
 
         // Hash password
@@ -51,7 +52,7 @@ const registerUser = async(req, res) => {
         })
 
         // Token JWT Register
-        const token = jwt.sign({ userId: newUser.id }, secretKey, {expiresIn: '1h' });
+        const token = jwt.sign({ userId: newUser.id }, secretKey, { expiresIn: '1h' });
         res.json({ message: 'Registration Successful', user: newUser, token })
 
     } catch (error) {
@@ -61,25 +62,25 @@ const registerUser = async(req, res) => {
 }
 
 // Login User
-const loginUsers = async(req, res) => {
+const loginUsers = async (req, res) => {
     const { email, password } = req.body;
 
-    if(!email || !password) {
+    if (!email || !password) {
         return res.status(400).json({ error: 'All must be filled' })
     }
-    
+
     try {
         const checkUser = await prisma.user.findUnique({
             where: { email },
         });
 
-        if(!checkUser) {
+        if (!checkUser) {
             return res.status(400).json({ error: 'Wrong email or password' })
         }
 
         const passwordMatch = await bcrypt.compare(password, checkUser.password)
 
-        if(passwordMatch) {
+        if (passwordMatch) {
             // Token JWT Login
             const token = jwt.sign({ userId: checkUser.id }, secretKey, { expiresIn: '1h' });
 
@@ -93,10 +94,55 @@ const loginUsers = async(req, res) => {
         res.status(400).json({ error: 'Login Failed' })
     }
 }
+// Send Email Forgot Password
+const sendEmailForgotPassword = async (nama, email) => {
+    let data = {
+        service_id: process.env.EMAILJS_SERVICE_ID,
+        template_id: process.env.EMAILJS_TEMPLATE_ID,
+        user_id: process.env.EMAILJS_USER_ID,
+        template_params: {
+            'nama': nama,
+            'to_email': email
+        },
+        'accessToken': process.env.EMAILJS_ACCESS_TOKEN
+    };
+
+    try {
+        await axios.post('https://api.emailjs.com/api/v1.0/email/send', data)
+
+        return true
+    } catch (error) {
+        console.log(error);
+        return false
+    }
+}
+
+// Forgot Password
+const forgotPassword = async (req, res) => {
+    const { email } = req.body
+
+    // TASK
+    // logic cek apakah email ada di database
+    // jika ada, kirim email
+
+    // params1: nama user
+    // params2: email user
+    const response = await sendEmailForgotPassword('Adam', 'raywibowo68@gmail.com')
+
+    if (response) {
+        return res.json({ message: 'Email sent!' })
+    } else {
+        return res.status(400).json({ error: 'Email not sent' })
+    }
+
+    // jika tidak ada, kirim response error
+
+}
 
 // jangan lupa export functionnya
 module.exports = {
     contohResponse,
     registerUser,
     loginUsers,
+    forgotPassword
 }
