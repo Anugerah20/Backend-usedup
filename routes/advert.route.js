b01bcd63a769afec164ec0681959f3739d0d9247
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-12-19 18:33:49 +0700

    feat(router) get all advert

--- routes/advert.route.js
+++ routes/advert.route.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const authenticateToken = require('../middleware/protect.route.middleware.js');
-const { createAdvert } = require('../controllers/iklan.controller.js');
+const { createAdvert, getAdvert } = require('../controllers/iklan.controller.js');
 
 
 // Route Advert
 router.post('/advert', authenticateToken, createAdvert);
+
+// show all product pagination
+router.get('/getAdvert', getAdvert);
 
 module.exports = router;
