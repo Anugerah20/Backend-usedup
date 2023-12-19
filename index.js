608bd4b9a18be1a24e6b819e1a60420d679c2ee9
Adam Ray <52489625+adamrayw@users.noreply.github.com>
2023-12-20 01:15:26 +0700

    add new env port

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ app.use('/api/category', categoryRoute)
 app.use('/api/advert', advertRoute)
 
 // ini untuk untuk memastikan server berjalan di port 3000
-app.listen(port, () => {
+app.listen(port || process.env.PORT, () => {
     console.log(`Server is running on port ${port}`);
 });
