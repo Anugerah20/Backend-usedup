1e73a45154155b0f56fc0713d96e5c8c6344e4ec
Adam Ray <52489625+adamrayw@users.noreply.github.com>
2024-02-08 10:01:21 +0700

    refactor: change category to additional, and add province endpoint

--- routes/additional.route.js
+++ routes/additional.route.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllCategory, createCategory } = require('../controllers/category.controller.js');
+const { getAllCategory, createCategory, getAllProvinces } = require('../controllers/additional.controller.js');
 const authenticateToken = require('../middleware/protect.route.middleware.js');
 
 // Route Get All Category
-router.get('/', getAllCategory);
+router.get('/category', getAllCategory);
 
 // Route Create Category
 router.post('/category', authenticateToken, createCategory);
+
+// Route Get Province
+router.get('/province', getAllProvinces)
 
 
 module.exports = router;
