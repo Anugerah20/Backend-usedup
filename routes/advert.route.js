0ef96a8c09d190cb77e4e9df303a7a23ed9b927c
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-02-08 11:40:04 +0700

    feat (route): new get, post and delete like advert

--- routes/advert.route.js
+++ routes/advert.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authenticateToken = require('../middleware/protect.route.middleware.js');
-const { createAdvert, getAdvert, getDetailAdvert } = require('../controllers/iklan.controller.js');
+const { createAdvert, getAdvert, getDetailAdvert, getLikeAdvert, createLikeAdvert, deleteLikeAdvert } = require('../controllers/iklan.controller.js');
 
 
 // Route Advert
@@ -12,5 +12,14 @@ router.get('/getAdvert', getAdvert);
 
 // Route detail advert
 router.get('/getDetailAdvert/:id', getDetailAdvert);
+
+// Route Get Like Advert
+router.get('/getLikeAdvert/:id', getLikeAdvert);
+
+// Route Create Like Advert
+router.post('/likeAdvert', authenticateToken, createLikeAdvert);
+
+// Route Delete Like Advert
+router.delete('/deleteLikeAdvert', authenticateToken, deleteLikeAdvert);
 
 module.exports = router;
