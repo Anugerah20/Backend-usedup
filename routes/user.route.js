7d75fb220bea5bf98e2541a7391c090b256ea70e
HafizhMA <muhammadazhr29@gmail.com>
2024-08-31 22:02:49 +0700

    setup verifikasi email

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,7 +5,7 @@ const authenticateToken = require('../middleware/protect.route.middleware.js')
 // tes
 
 // ini import function dari controller
-const { registerUser, loginUsers, forgotPassword, checkToken, updatePassword, createAdvert, createCategory, showDataUser, editProfile } = require('../controllers/user.controller.js')
+const { registerUser, loginUsers, forgotPassword, checkToken, updatePassword, createAdvert, createCategory, showDataUser, editProfile, verifAccount, confirmVerifAccout } = require('../controllers/user.controller.js')
 
 // Route Register
 router.post('/register', registerUser);
@@ -14,17 +14,21 @@ router.post('/register', registerUser);
 router.post('/login', loginUsers);
 
 // Route forgot password
-router.post('/forgot-password', authenticateToken,forgotPassword);
+router.post('/forgot-password', authenticateToken, forgotPassword);
 
 // Route check token
-router.post('/check-token', authenticateToken,checkToken);
+router.post('/check-token', authenticateToken, checkToken);
 
 // Route updated password
-router.post('/update-password', authenticateToken,updatePassword);
+router.post('/update-password', authenticateToken, updatePassword);
 
 // Route Get User
-router.get("/:id", authenticateToken,showDataUser);
+router.get("/:id", authenticateToken, showDataUser);
 
-router.put("/edit-profile/:id", authenticateToken,editProfile);
+router.put("/edit-profile/:id", authenticateToken, editProfile);
+
+router.post("/verifAccount", authenticateToken, verifAccount);
+
+router.put("/confirmVerifAccount/:token", authenticateToken, confirmVerifAccout)
 
 module.exports = router;
