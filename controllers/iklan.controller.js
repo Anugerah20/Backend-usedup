977eb6ead607676e996aaec1ac7ef8940451af0d
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-09-20 14:09:15 +0700

    Refactor Advert model to use address field instead of location

--- controllers/iklan.controller.js
+++ controllers/iklan.controller.js
@@ -5,7 +5,7 @@ const prisma = new PrismaClient()
 
 // Advert Produk
 const createAdvert = async (req, res) => {
-    let { title, description, price, image, location, latitude, longitude, categoryId, provinceId, userId } = req.body
+    let { title, description, price, image, address, latitude, longitude, categoryId, provinceId, userId } = req.body
     price = parseInt(price)
 
     try {
@@ -15,7 +15,7 @@ const createAdvert = async (req, res) => {
                 description,
                 price,
                 image,
-                location,
+                address,
                 latitude,
                 longitude,
                 categoryId,
